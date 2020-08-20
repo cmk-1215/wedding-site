d677dd88b6b06000d5e8d46f6be26c0c73ee5014
Christina Kurpiel <kurpielcm@gmail.com>
2020-08-20 16:32:32 -0400

    add middleware, reduxpromise, and createStore to index

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import ReduxPromise from "redux-promise";
 
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
