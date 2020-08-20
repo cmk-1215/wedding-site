6c981fac4a0d9cc08ba391d84cf6e41574d8b39f
Christina Kurpiel <kurpielcm@gmail.com>
2020-08-20 16:15:31 -0400

    initial react-redux, and node file setup

---
+++ server/client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          WEDDING SITE
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
